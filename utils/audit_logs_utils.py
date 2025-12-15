aae2278f8fc1ab7bdcff9d83fa8f82db11332c1a
otbludo <otabelaludovic6@gmail.com>
2025-12-15 15:14:56 +0100

    modif sorti des logs

--- utils/audit_logs_utils.py
+++ utils/audit_logs_utils.py
@@ -1,8 +1,7 @@
 from sqlalchemy.future import select
-from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.models import AuditLog
-
+from datetime import datetime, timedelta, timezone
 
 class auditLogsCRUD:
     def __init__(self, db: AsyncSession):
@@ -14,8 +13,26 @@ class auditLogsCRUD:
                 result = await self.db.execute(select(AuditLog))
                 logs = result.scalars().all()
 
-            return logs
-        
+            # Dates de référence (aware UTC)
+            now = datetime.now(timezone.utc)
+            today_start = datetime(now.year, now.month, now.day, tzinfo=timezone.utc)
+            last_7_days_start = today_start - timedelta(days=7)
+
+            # Pour le mois précédent
+            first_day_this_month = datetime(now.year, now.month, 1, tzinfo=timezone.utc)
+            last_day_last_month = first_day_this_month - timedelta(seconds=1)
+            first_day_last_month = datetime(last_day_last_month.year, last_day_last_month.month, 1, tzinfo=timezone.utc)
+
+            # Filtrage
+            logs_dict = {
+                "all": logs,
+                "today": [log for log in logs if log.timestamp >= today_start],
+                "last_7_days": [log for log in logs if log.timestamp >= last_7_days_start],
+                "last_month": [log for log in logs if first_day_last_month <= log.timestamp <= last_day_last_month],
+            }
+
+            return logs_dict
+
         except Exception as e:
             await self.db.rollback()
             raise e
--- utils/stat_utils.py
+++ utils/stat_utils.py
@@ -1,7 +1,7 @@
 from sqlalchemy.future import select
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
-from db.models import Customer, Admin, Devis
+from db.models import Customer, CustomerDeleted, Admin, AdminDeleted, Devis, DevisDeleted, ProductPrinting, ProductPrintingDeleted
 
 
 class StatsCRUD:
@@ -9,47 +9,110 @@ class StatsCRUD:
         self.db = db
 
     async def get_stats(self):
-        # Total Customers
-        total_customers_query = await self.db.execute(
-            select(func.count(Customer.id))
-        )
-        total_customers = total_customers_query.scalar()
-
-        # Total Admins
+        
+        #--------------------------------------------------------------------------------------
+        # Admins
+        #--------------------------------------------------------------------------------------
+        
         total_admins_query = await self.db.execute(
             select(func.count(Admin.id))
         )
         total_admins = total_admins_query.scalar()
 
-        # # Total Devis
-        # total_devis_query = await self.db.execute(
-        #     select(func.count(Devis.id))
-        # )
-        # total_devis = total_devis_query.scalar()
+        total_admin_by_status_query = await self.db.execute(
+            select(Admin.status, func.count(Admin.id))
+            .group_by(Admin.status)
+        )
+        total_admins_by_status = {
+            status: count for status, count in total_admin_by_status_query.all()
+        }
 
-        # # Devis by status
-        # devis_by_status_query = await self.db.execute(
-        #     select(Devis.status, func.count(Devis.id))
-        #     .group_by(Devis.status)
-        # )
-        # devis_by_status = {
-        #     status: count for status, count in devis_by_status_query.all()
-        # }
+        total_admins_deleted_query = await self.db.execute(
+            select(func.count(AdminDeleted.id))
+        )
+        total_admins_deleted = total_admins_deleted_query.scalar()
+        
+        
+        #--------------------------------------------------------------------------------------
+        # Customers
+        #--------------------------------------------------------------------------------------
 
-        # Customers by status
-        customers_by_status_query = await self.db.execute(
+        total_customers_query = await self.db.execute(
+            select(func.count(Customer.id))
+        )
+        total_customers = total_customers_query.scalar()
+        
+        total_customers_by_status_query = await self.db.execute(
             select(Customer.status, func.count(Customer.id))
             .group_by(Customer.status)
         )
-        customers_by_status = {
-            status: count for status, count in customers_by_status_query.all()
+        total_customers_by_status = {
+            status: count for status, count in total_customers_by_status_query.all()
         }
 
-        # Return dict structure (StatsResponse va convertir automatiquement)
+        total_customers_deleted_query = await self.db.execute(
+            select(func.count(CustomerDeleted.id))
+        )
+        total_customers_deleted = total_customers_deleted_query.scalar()
+        
+        
+        #--------------------------------------------------------------------------------------
+        # Products
+        #--------------------------------------------------------------------------------------
+
+        total_products_query = await self.db.execute(
+            select(func.count(ProductPrinting.id))
+        )
+        total_products = total_products_query.scalar()
+
+        total_products_deleted_query = await self.db.execute(
+            select(func.count(ProductPrintingDeleted.id))
+        )
+        total_products_deleted = total_products_deleted_query.scalar()
+        
+        
+        #--------------------------------------------------------------------------------------
+        # Devis
+        #--------------------------------------------------------------------------------------
+        
+        total_devis_query = await self.db.execute(
+            select(func.count(Devis.id))
+        )
+        total_devis = total_devis_query.scalar()
+        
+        devis_by_status_query = await self.db.execute(
+            select(Devis.status, func.count(Devis.id))
+            .group_by(Devis.status)
+        )
+        devis_by_status = {
+            status: count for status, count in devis_by_status_query.all()
+        }
+        
+        total_devis_deleted_query = await self.db.execute(
+            select(func.count(DevisDeleted.id))
+        )
+        total_devis_deleted = total_devis_deleted_query.scalar()
+
+
         return {
-            "total_customers": total_customers,
-            "total_admins": total_admins,
-            # "total_devis": total_devis,
-            # "devis_by_status": devis_by_status,
-            "customers_by_status": customers_by_status
+            "admins":{
+                "total_admins": total_admins,
+                "total_admins_by_status": total_admins_by_status,
+                "total_admins_deleted": total_admins_deleted
+            },
+            "customers":{
+                "total_customers": total_customers,
+                "total_customers_by_status": total_customers_by_status,
+                "total_customers_deleted": total_customers_deleted
+            },
+            "products":{
+                "total_products": total_products,
+                "total_products_deleted": total_products_deleted
+            },
+            "devis":{
+                "total_devis": total_devis,
+                "total_devis_by_status": devis_by_status,
+                "devis_deleted": total_devis_deleted
+            },
+            
         }
